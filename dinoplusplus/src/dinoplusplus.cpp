1af69b9057a41461aab3583c531c8609e6b53829
pasmoi <pm@debian-PM>
2017-09-24 03:48:44 +0200

    aaaa

--- dinoplusplus/src/dinoplusplus.cpp
+++ dinoplusplus/src/dinoplusplus.cpp
@@ -7,7 +7,7 @@ int main()
 {
   ListeChainee *liste;
 
-  //petit test de git
+  //petit test de git aaa
   liste = new ListeChainee();
   cout << "testeee: " << liste << endl; // prints !!!Hello World!!!
   liste->AfficheDebug();
