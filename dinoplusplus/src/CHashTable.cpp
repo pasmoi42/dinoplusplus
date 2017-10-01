9f8ad5d2fbe29de6df66103e84d5ebd1815dfdbd
pasmoi <pm@debian-PM>
2017-10-01 22:18:49 +0200

    aaaa

---
+++ dinoplusplus/src/CHashTable.cpp
@@ -0,0 +1,20 @@
+/*
+ * CHashTable.cpp
+ *
+ *  Created on: 1 oct. 2017
+ *      Author: pm
+ */
+
+#include "CHashTable.h"
+
+CHashTable::CHashTable()
+{
+  // TODO Auto-generated constructor stub
+
+}
+
+CHashTable::~CHashTable()
+{
+  // TODO Auto-generated destructor stub
+}
+
---
+++ dinoplusplus/src/CHashTable.h
@@ -0,0 +1,28 @@
+/*
+ * CHashTable.h
+ *
+ *  Created on: 1 oct. 2017
+ *      Author: pm
+ */
+
+#ifndef CHASHTABLE_H_
+#define CHASHTABLE_H_
+
+#include <memory>
+
+class C2Links
+{
+  std::shared_ptr<C2Links> suivant;
+  std::shared_ptr<C2Links> precedent;
+};
+
+class CHashTable
+{
+//private:
+  //std::vector<std::pair>
+public:
+  CHashTable();
+  virtual ~CHashTable();
+};
+
+#endif /* CHASHTABLE_H_ */
