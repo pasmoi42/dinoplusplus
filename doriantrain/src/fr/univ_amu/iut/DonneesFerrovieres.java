51ed0b8dec4d5356906e4fbb85cbde5e0c8dfcf7
pasmoi <pm@debian-PM>
2017-11-19 01:20:23 +0100

    bug git

---
+++ doriantrain/src/fr/univ_amu/iut/DonneesFerrovieres.java
@@ -0,0 +1,27 @@
+package fr.univ_amu.iut;
+
+/**
+ * Toutes les donnes utilisees par ce logiciel de simulation. Constitue le
+ * Modele du pattern MVC autour du quel est achitecture ce logiciel.
+ * 
+ * 
+ * @author Dorian
+ *
+ */
+public class DonneesFerrovieres {
+	
+	private DonneesFerrovieres() {
+		
+	}
+	
+	private static DonneesFerrovieres leSeulEtUniqueModele = null;
+
+	/** seule et unique maniere d'acceder a notre singleton-controlleur */
+	public static DonneesFerrovieres accedeAuxDonnees() {
+		if (leSeulEtUniqueModele == null) {
+			leSeulEtUniqueModele = new DonneesFerrovieres();
+		}
+		return leSeulEtUniqueModele;
+	}
+
+}
