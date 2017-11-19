bf60b725810147ee5a7c016eefa8f1da8108776c
pasmoi <pm@debian-PM>
2017-11-19 01:47:58 +0100

    VueEnssembleReseau

---
+++ doriantrain/src/fr/univ_amu/iut/VueEnssembleReseau.java
@@ -0,0 +1,34 @@
+package fr.univ_amu.iut;
+
+/**
+ * d'apres le sujet:
+ * Une vue qui affiche l'ensemble du reseau à l'etat actuel : les trains et leur situation dans le reseau, les gares
+ * 
+ * @author Dorian
+ *
+ */
+public class VueEnssembleReseau  implements VueGenerique {
+	
+	@Override
+	public void initialiseAffichage() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void prendEnCompteNouvellesDonnes() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void metAJourAffichage() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void fermeAffichage() {
+		// TODO Auto-generated method stub
+	}
+}
