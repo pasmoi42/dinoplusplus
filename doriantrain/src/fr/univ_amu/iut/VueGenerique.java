818e29078b39a938cd727ec616aab11340532753
pasmoi <pm@debian-PM>
2017-11-19 01:27:41 +0100

    VueGenerique

--- doriantrain/src/fr/univ_amu/iut/VueGenerique.java
+++ doriantrain/src/fr/univ_amu/iut/VueGenerique.java
@@ -2,9 +2,21 @@ package fr.univ_amu.iut;
 
 /**
  * 
+ * Ce que doit faire toute "vue" (en gros tout affichage), 
+ * pour fonctionner.
+ * 
+ * 
  * @author dorian
  *
  */
-public interface VueGenerique {
-
+public interface VueGenerique 
+{
+	
+	/**
+	 * Va regarder ce qui a change dans le modele (DonneesFerrovieres)
+	 * pour le prendre en comtpe pour "l'affichage".
+	 */
+	public void prendEnCompteNouvellesDonnes();
+	
+	
 }
