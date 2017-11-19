dab6df695db090b9ca93ead4b3c9eacf54d63dd2
DorianTachet <Dorian.tachet>
2017-11-19 07:12:47 +0100

    Fabrique train

--- doriantrain/src/fr/univ_amu/iut/FabriqueTrain.java
+++ doriantrain/src/fr/univ_amu/iut/FabriqueTrain.java
@@ -1,11 +1,20 @@
 package fr.univ_amu.iut;
 
+import java.util.Collection;
+import java.util.LinkedList;
+
 public class FabriqueTrain {
+    
+    private int largeur,longueur,taille,vitesseMax;
+    private EntrepriseFerroviaire proprietaire;
+    private Collection<Wagon> wagons = new LinkedList<>();
 
     public static Train getNouveauTrainBetail () {
         Train trainBetail;
 
-        return trainBetail = new TrainBetail();
+        trainBetail = new TrainBetail();
+
+        return trainBetail;
     }
 
 
--- doriantrain/src/fr/univ_amu/iut/Train.java
+++ doriantrain/src/fr/univ_amu/iut/Train.java
@@ -4,7 +4,7 @@ import java.util.Collection;
 import java.util.LinkedList;
 
 public abstract class Train {
-    private int largeur,longueur,taille;
+    private int largeur,longueur,taille,vitesseMax;
     private EntrepriseFerroviaire proprietaire;
     private Collection<Wagon> wagons = new LinkedList<>();
 
